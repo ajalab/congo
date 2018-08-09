a45cfe969f50131a27a178ec7633628cf82d3ee9
Koki Kato <koki.kato1994@gmail.com>
2018-08-09 17:30:54 +0900

    Add ZeroValue

--- cmd/congo/runner.go
+++ cmd/congo/runner.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"go/types"
 	"os"
+	"unsafe"
 
 	"github.com/ajalab/congo/cmd/congo/interp"
 	"golang.org/x/tools/go/ssa"
@@ -21,7 +23,11 @@ func (program *Program) Run() error {
 	n := len(program.symbols)
 	symbolValues := make([]interp.SymbolicValue, n)
 	for i := 0; i < n; i++ {
-		symbolValues[i] = interp.SymbolicValue{Value: int32(0), Type: program.symbols[i]}
+		ty := program.symbols[i]
+		symbolValues[i] = interp.SymbolicValue{
+			Value: ZeroValue(ty),
+			Type:  ty,
+		}
 	}
 
 	program.mainFunc.WriteTo(os.Stdout)
@@ -36,4 +42,90 @@ func (program *Program) Run() error {
 		"",
 		[]string{})
 	return nil
+}
+
+func ZeroValue(ty types.Type) interface{} {
+	switch t := ty.(type) {
+	case *types.Basic:
+		if t.Kind() == types.UntypedNil {
+			panic("untyped nil has no zero value")
+		}
+		switch t.Kind() {
+		case types.Bool:
+			return false
+		case types.Int:
+			return int(0)
+		case types.Int8:
+			return int8(0)
+		case types.Int16:
+			return int16(0)
+		case types.Int32:
+			return int32(0)
+		case types.Int64:
+			return int64(0)
+		case types.Uint:
+			return uint(0)
+		case types.Uint8:
+			return uint8(0)
+		case types.Uint16:
+			return uint16(0)
+		case types.Uint32:
+			return uint32(0)
+		case types.Uint64:
+			return uint64(0)
+		case types.Uintptr:
+			return uintptr(0)
+		case types.Float32:
+			return float32(0)
+		case types.Float64:
+			return float64(0)
+		case types.Complex64:
+			return complex64(0)
+		case types.Complex128:
+			return complex128(0)
+		case types.String:
+			return ""
+		case types.UnsafePointer:
+			return unsafe.Pointer(nil)
+		default:
+			panic(fmt.Sprint("zero for unexpected type:", t))
+		}
+	case *types.Pointer:
+		return (*interface{})(nil)
+	case *types.Array:
+		a := make([]interface{}, t.Len())
+		for i := range a {
+			a[i] = ZeroValue(t.Elem())
+		}
+		return a
+	case *types.Named:
+		return ZeroValue(t.Underlying())
+	case *types.Interface:
+		panic("unimplemented")
+	case *types.Slice:
+		return []interface{}(nil)
+	case *types.Struct:
+		s := make([]interface{}, t.NumFields())
+		for i := range s {
+			s[i] = ZeroValue(t.Field(i).Type())
+		}
+		return s
+	case *types.Tuple:
+		if t.Len() == 1 {
+			return ZeroValue(t.At(0).Type())
+		}
+		s := make([]interface{}, t.Len())
+		for i := range s {
+			s[i] = ZeroValue(t.At(i).Type())
+		}
+		return s
+	case *types.Chan:
+		return chan interface{}(nil)
+	case *types.Map:
+		return map[interface{}][]interface{}(nil)
+	case *types.Signature:
+		return (*ssa.Function)(nil)
+	}
+
+	panic(fmt.Sprint("zero: unexpected ", ty))
 }
