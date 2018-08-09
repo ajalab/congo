64c8e45c7bb6c8fa9d21c79792ad50b4a32b5470
Koki Kato <koki.kato1994@gmail.com>
2018-08-09 17:49:20 +0900

    Fix test cases

--- testdata/branch_and.go
+++ testdata/branch_and.go
@@ -2,7 +2,7 @@ package testdata
 
 import "fmt"
 
-func branch_and(x int32) {
+func BranchAnd(x int32) {
 	if 0 < x && x < 5 {
 		fmt.Printf("x is in 0 ~ 5")
 	} else {
--- testdata/branch_simple.go
+++ testdata/branch_simple.go
@@ -3,8 +3,8 @@ package testdata
 import "fmt"
 
 func BranchSimple(x int32) {
-	if 0 < x && x < 5 {
-		fmt.Printf("x is in 0 ~ 5")
+	if x < 5 {
+		fmt.Printf("x is smaller than 5")
 	} else {
 		fmt.Printf("other")
 	}
