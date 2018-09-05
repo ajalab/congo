baabbff424f03c8c0013dc3fed138e6328623b73
Koki Kato <koki.kato1994@gmail.com>
2018-09-05 16:48:04 +0900

    Add new tests

--- congo_test.go
+++ congo_test.go
@@ -44,3 +44,37 @@ func TestRun(t *testing.T) {
 		}
 	}
 }
+
+func TestExecute(t *testing.T) {
+
+	testCases := []struct {
+		packageName string
+		funcName    string
+		maxExec     uint
+		minCoverage float64
+	}{
+		{"github.com/ajalab/congo/testdata", "Max2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "Min2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "Max3", 5, 1},
+	}
+
+	for _, tc := range testCases {
+		conf := Config{
+			PackageName: tc.packageName,
+			FuncName:    tc.funcName,
+		}
+
+		prog, err := conf.Open()
+		if err != nil {
+			t.Fatalf("Config.Open: %v\n", err)
+		}
+
+		res, err := prog.Execute(tc.maxExec, tc.minCoverage)
+		if err != nil {
+			t.Fatalf("Program.Execute: %v\n", err)
+		}
+		if res.Coverage < tc.minCoverage {
+			t.Fatalf("coverage could not be accomplished: expected %f, actual %f\n", tc.minCoverage, res.Coverage)
+		}
+	}
+}
