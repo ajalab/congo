c34805da4ba7578370d8d953b11c83b065239fbe
Koki Kato <koki.kato1994@gmail.com>
2018-09-12 16:55:07 +0900

    Add -cpuprofile option

--- cmd/congo/main.go
+++ cmd/congo/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/ajalab/congo"
 )
 
-func main() {
-	var packageName, funcName string
-	if len(os.Args) <= 2 {
-		packageName = "github.com/ajalab/congo/cmd/congo/testdata"
-		funcName = "BranchThreeVars"
-	} else {
-		packageName = os.Args[1]
-		funcName = os.Args[2]
+var (
+	cpuProfile  = flag.String("cpuprofile", "", "write cpu profile to file")
+	minCoverage = flag.Float64("coverage", 1.0, "minimum coverage")
+	maxExec     = flag.Uint("maxexec", 10, "maximum execution time")
+)
 
+func main() {
+	flag.Parse()
+
+	var packageName, funcName string
+	if flag.NArg() < 2 {
+		packageName = "github.com/ajalab/congo/testdata"
+		funcName = "BranchSimple"
+	} else {
+		packageName = flag.Arg(0)
+		funcName = flag.Arg(1)
 	}
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	conf := congo.Config{
 		PackageName: packageName,
 		FuncName:    funcName,
@@ -28,7 +47,10 @@ func main() {
 		log.Fatalf("Config.Open: %v", err)
 	}
 
-	if err = prog.Execute(5, 1.0); err != nil {
+	if result, err := prog.Execute(*maxExec, *minCoverage); err != nil {
 		fmt.Println("failed: ", err)
+	} else {
+		fmt.Println(result)
 	}
+
 }
