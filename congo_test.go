d0fd05b8b7fe93a51aa925763204100e0bd7dea0
Koki Kato <k-kato@nii.ac.jp>
2018-09-27 16:32:59 +0900

    Support string types (equality, concatenation)

--- congo_test.go
+++ congo_test.go
@@ -39,24 +39,14 @@ func TestRun(t *testing.T) {
 	}
 }
 
-func TestExecute(t *testing.T) {
+type executeTestCase struct {
+	packageName string
+	funcName    string
+	maxExec     uint
+	minCoverage float64
+}
 
-	testCases := []struct {
-		packageName string
-		funcName    string
-		maxExec     uint
-		minCoverage float64
-	}{
-		{"github.com/ajalab/congo/testdata", "Max2", 2, 1},
-		{"github.com/ajalab/congo/testdata", "Min2", 2, 1},
-		{"github.com/ajalab/congo/testdata", "Max3", 4, 1},
-		{"github.com/ajalab/congo/testdata", "Min3", 4, 1},
-		{"github.com/ajalab/congo/testdata", "UMax2", 2, 1},
-		{"github.com/ajalab/congo/testdata", "UMin2", 2, 1},
-		{"github.com/ajalab/congo/testdata", "UMax3", 4, 1},
-		{"github.com/ajalab/congo/testdata", "UMin3", 4, 1},
-	}
-
+func testExecute(testCases []executeTestCase, t *testing.T) {
 	for _, tc := range testCases {
 		conf := Config{
 			PackageName: tc.packageName,
@@ -76,4 +66,26 @@ func TestExecute(t *testing.T) {
 			t.Fatalf("%+v\ncoverage could not be accomplished: expected %f, actual %f\n", tc, tc.minCoverage, res.Coverage)
 		}
 	}
+}
+
+func TestExecuteMaxMin(t *testing.T) {
+	testCases := []executeTestCase{
+		{"github.com/ajalab/congo/testdata", "Max2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "Min2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "Max3", 4, 1},
+		{"github.com/ajalab/congo/testdata", "Min3", 4, 1},
+		{"github.com/ajalab/congo/testdata", "UMax2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "UMin2", 2, 1},
+		{"github.com/ajalab/congo/testdata", "UMax3", 4, 1},
+		{"github.com/ajalab/congo/testdata", "UMin3", 4, 1},
+	}
+	testExecute(testCases, t)
+}
+
+func TestExecuteString(t *testing.T) {
+	testCases := []executeTestCase{
+		{"github.com/ajalab/congo/testdata", "IsABC", 2, 1},
+		{"github.com/ajalab/congo/testdata", "IsABCIfConcatenated", 2, 1},
+	}
+	testExecute(testCases, t)
 }
