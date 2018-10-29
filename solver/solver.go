47851312158b8e888cb0d610d6e372fb879396c1
Koki Kato <koki.kato1994@gmail.com>
2018-10-29 19:00:16 +0900

    Add solver.Branch interface

--- solver/solver.go
+++ solver/solver.go
@@ -2,11 +2,35 @@ package solver
 
 import "golang.org/x/tools/go/ssa"
 
-// IfBranch contains a branching instruction (*ssa.If) and
+// Branch represents a branching instruction that appeared in a running trace with some additional information.
+// This includes instructions that may cause a panic (e.g., pointer dereference) as well as ordinary branching by *ssa.If.
+type Branch interface {
+	// Instr returns ssa.Instruction value for the branch.
+	Instr() ssa.Instruction
+}
+
+// BranchIf contains a branching instruction (*ssa.If) and
 // the direction taken in the concolic execution.
-type IfBranch struct {
-	Instr     *ssa.If
+type BranchIf struct {
+	instr     *ssa.If
 	Direction bool
+}
+
+func (b *BranchIf) Instr() ssa.Instruction {
+	return b.instr
+}
+
+func (b *BranchIf) Succs() []*ssa.BasicBlock {
+	return b.instr.Block().Succs
+}
+
+// PanicNilPointerDeref represents a (panic) branching caused by nil pointer dereference.
+type PanicNilPointerDeref struct {
+	instr *ssa.UnOp
+}
+
+func (p *PanicNilPointerDeref) Instr() ssa.Instruction {
+	return p.instr
 }
 
 // UnsatError is an error describing that Z3 constraints were unsatisfied.
