eae0f3c2a4895a4956e36dfac2a95c25713cbced
Koki Kato <koki.kato1994@gmail.com>
2018-10-29 19:08:58 +0900

    Merge gen.go and value.go

--- value.go
+++ value.go
@@ -2,7 +2,10 @@ package congo
 
 import (
 	"fmt"
+	"go/ast"
+	"go/token"
 	"go/types"
+	"strconv"
 	"unsafe"
 
 	"golang.org/x/tools/go/ssa"
@@ -92,4 +95,50 @@ func zero(ty types.Type) interface{} {
 	}
 
 	panic(fmt.Sprint("zero: unexpected ", ty))
+}
+
+func type2ASTExpr(ty types.Type) ast.Expr {
+	switch ty := ty.(type) {
+	case *types.Basic:
+		return ast.NewIdent(ty.Name())
+	case *types.Named:
+		return &ast.SelectorExpr{
+			X:   ast.NewIdent(ty.Obj().Pkg().Name()),
+			Sel: ast.NewIdent(ty.Obj().Id()),
+		}
+	case *types.Pointer:
+		return &ast.StarExpr{
+			X: type2ASTExpr(ty.Elem()),
+		}
+	default:
+		panic("unimplemented")
+	}
+}
+
+func value2ASTExpr(v interface{}, ty types.Type) ast.Expr {
+	switch ty := ty.(type) {
+	case *types.Basic:
+		info := ty.Info()
+		switch {
+		case info&types.IsBoolean > 0:
+			s := "false"
+			if v.(bool) {
+				s = "true"
+			}
+			return ast.NewIdent(s)
+		case info&types.IsInteger > 0:
+			return &ast.BasicLit{
+				Kind:  token.INT,
+				Value: fmt.Sprintf("%v", v),
+			}
+		case info&types.IsString > 0:
+			return &ast.BasicLit{
+				Kind:  token.STRING,
+				Value: strconv.Quote(v.(string)),
+			}
+		default:
+			panic("unimplemented")
+		}
+	}
+	panic("unimplemented")
 }
